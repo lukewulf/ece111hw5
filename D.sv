e869fb8e4116100311b0332102daf1573f693499
alex haggart <alex.haggart@gmail.com>
2018-11-20 15:24:37 -0800

    instruction decode done, testbench is clean

---
+++ D.sv
@@ -0,0 +1,93 @@
+// Instruction Decode (D) module
+import definitions::*;
+
+module D(
+    input wire        clk,
+    input Signal      reset,
+    input Register    rd_i,
+    input Instruction instr,
+
+    output Register   rs_o,
+    output Register   rt_o
+);
+
+Signal write;
+
+RegAddr rs, rt, rd;
+
+RType instr_r;
+assign instr_r = RType'(instr);
+
+IType instr_i;
+assign instr_i = IType'(instr);
+
+JType instr_j;
+assign instr_j = JType'(instr);
+
+OpCode [5:0] opcode;
+assign opcode = OpCode'(instr[31:26]);
+
+RegisterFile RF(
+    .clk(clk),
+    .reset(reset),
+
+    .write(write),
+    .rd_i(rd_i),
+
+    .rs(rs),
+    .rt(rt),
+    .rd(rd),
+
+    .rs_o(rs_o),
+    .rt_o(rt_o)
+);
+
+always_comb begin
+    case(opcode)
+        RTYPE: begin  //  R[rd] = R[rs] + R[rt] 
+            rs    = instr_r.rs;
+            rt    = instr_r.rt;
+            rd    = instr_r.rd;
+            write = ENABLE;
+        end
+
+        ADDI: begin  //  R[rt] = R[rs] + SignExtImm 
+            rs    = instr_i.rs;
+            rt    = RegAddr'(0);  // dont care
+            rd    = instr_i.rt; // use rt as the write register
+            write = ENABLE;
+        end
+
+        BEQ: begin  // if(R[rs]==R[rt]) PC=PC+4+BranchAddr
+                    // expose rs and rt
+            rs    = instr_i.rs;
+            rt    = instr_i.rt;
+            rd    = RegAddr'(0);  // dont care
+            write = DISABLE;
+        end
+
+        LW: begin  // R[rt] = M[R[rs]+SignExtImm]
+            rs    = instr_i.rs;
+            rt    = RegAddr'(0);  // dont care
+            rd    = instr_i.rt;
+            write = ENABLE;
+        end
+
+        SW: begin  //  M[R[rs]+SignExtImm] = R[rt]
+            rs    = instr_i.rs;
+            rt    = instr_i.rt;
+            rd    = RegAddr'(0);  // dont care
+            write = DISABLE;
+        end
+
+        default: begin // J
+            // nothing even matters
+            rs    = RegAddr'(0);
+            rt    = RegAddr'(0);
+            rd    = RegAddr'(0);
+            write = DISABLE;
+        end
+    endcase
+end
+
+endmodule
--- definitions.sv
+++ definitions.sv
@@ -1,13 +1,13 @@
 //This file defines the parameters used in the alu
 package definitions;
     
-typedef enum logic[1:0] {
-//Our operations
-  OR = 2'b00,
-  AND = 2'b01,
-  ADD = 2'b10,
-  SUB = 2'b11
-} op_code;
+// typedef enum logic[1:0] {
+// //Our operations
+//   OR = 2'b00,
+//   AND = 2'b01,
+//   ADD = 2'b10,
+//   SUB = 2'b11
+// } op_code;
 
 typedef enum logic {
   ENABLE  = 1'b1,
@@ -19,26 +19,41 @@ parameter RegAddrWidth = 5;
 typedef logic[RegAddrWidth-1:0]  RegAddr;
 typedef logic[31:0] Register;
 
+typedef enum logic[5:0]{
+  RTYPE = 6'h00,  // ADD, SUB, AND, OR
+  J     = 6'h02,
+  BEQ   = 6'h04,
+  ADDI  = 6'h08,
+  LW    = 6'h23,
+  SW    = 6'h2b
+} OpCode;
+
+typedef enum logic[5:0]{
+  ADD = 6'h20,
+  SUB = 6'h22,
+  AND = 6'h24,
+  OR  = 6'h25
+} Funct;
+
 typedef logic[7:0]  ProgramCounter;
 typedef logic[31:0] Instruction;
-typedef logic[5:0]  OpCode;
 typedef logic[4:0]  ShAmt;
 typedef logic[15:0] Immediate;
 typedef logic[25:0] JumpAddr;
 
 // decompose an Instruction into R-Type components
 typedef struct packed {
-  OpCode   opcode;
+  OpCode  opcode;
   RegAddr rs;
   RegAddr rt;
   RegAddr rd;
-  ShAmt    shamt;
-  OpCode   funct;
+  ShAmt   shamt;
+  Funct   funct;
 } RType;
 
 // decompose an Instruction into I-Type components
 typedef struct packed {
-  OpCode    opcode;
+  OpCode   opcode;
   RegAddr  rs;
   RegAddr  rt;
   Immediate imm;
