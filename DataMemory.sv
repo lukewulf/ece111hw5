e5c4667cf73991e71b650c1f24b8b6bcda6f8beb
alex haggart <alex.haggart@gmail.com>
2018-11-20 16:02:18 -0800

    rewrote data memory

---
+++ DataMemory.sv
@@ -0,0 +1,25 @@
+import definitions::*;
+module DataMemory(
+  input  wire     clk,
+  input  Register addr,
+  input  Signal   read,
+  input  Signal   write,
+  input  Register mem_in,
+  output Register mem_out
+);
+
+Register mem [2**MemAddrWidth];
+
+wire [MemAddrWidth-1:0] _addr;
+assign _addr = add[MemAddrWidth-1:0];
+
+always_comb begin
+    if (read == ENABLE) mem_out = mem[_addr];
+    else                mem_out = Register'(32'bZ);
+end
+
+always_ff @ (posedge clk) begin
+    if (write == ENABLE) mem[_addr] = mem_in;
+end
+
+endmodule
