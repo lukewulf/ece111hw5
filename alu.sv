c2bb8850a8eedbc8afaed1aa8744866dba45c65b
Luke W Wulf <lwulf@UCSD.EDU>
2018-11-20 16:41:49 -0800

    style

--- alu.sv
+++ alu.sv
@@ -1,41 +1,33 @@
-// Create Date:     2017.05.05
-// Latest rev date: 2017.10.26
-// Created by:      J Eldon
-// Design Name:     CSE141L
-// Module Name:     ALU (Arithmetic Logical Unit)
 
-
-//This is the ALU module of the core, op_code_e is defined in definitions.v file
-// Includes new enum op_mnemonic to make instructions appear literally on waveform.
 import definitions::*;
 	 
-module alu (	input  [31:0] rs_i,	 // operand s
-            	input  [31:0] rt_i,	 // operand t
-            	input  op_code op_i,	 // instruction / opcode
-           	output logic [31:0] result_o,	 // rslt
+module alu (	input  [31:0] rs_i,
+            	input  [31:0] rt_i,
+            	input  op_code op_i,
+           	output logic [31:0] result_o,
 		output logic zero);
 		
 
 
-op_code op3; 	                     // type is op_code, as defined
-assign op3 = op_i;		       // Assuming that controller takes care of ALUop signal
+op_code op3; 	                   
+assign op3 = op_i;		 
 
-always_comb								  // no registers, no clocks
+always_comb							
   begin
-	result_o = 'd0;                 // default or NOP result
+	result_o = 'd0;        
   
   
-  case (op3)   						      // using top 3 bits as ALU instructions
+  case (op3)   						
 
-	OR: begin					//Simple XOR
+	OR: begin					
 		result_o = rs_i | rt_i;
 		end
 
-	AND: begin					//Simple AND
+	AND: begin					
 		result_o = rs_i & rt_i;
 		end
 
-	ADD: begin					// Addition
+	ADD: begin					
 		result_o = rs_i + rt_i;
 		end
 
